ae7ff9a3159cc5302041a03e438c96b0fe19acfa
Sasi <sasidharan0x@gmail.com>
2025-06-18 22:30:51 +0530

    Minor Change

--- schemas.py
+++ schemas.py
@@ -2,15 +2,15 @@ from marshmallow import Schema, fields
 
 class UserSchema(Schema):
     name = fields.String(required=True)
-    user_id = fields.String(dump_only=True)
+    user_id = fields.Integer(dump_only=True)
 
 
 class TaskSchema(Schema):
     title = fields.String(required=True)
-    task_id = fields.String(dump_only=True)
-    user_id = fields.String(required=True)
+    task_id = fields.Integer(dump_only=True)
+    user_id = fields.Integer(required=True)
 
 class TaskUpdateSchema(Schema):
     title = fields.String(required=True)
-    user_id = fields.String(dump_only=True)
+    user_id = fields.Integer(dump_only=True)
 
